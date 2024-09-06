c6b000f9d823270e87c4da9fc4a6aa1a8d4967c4
AndrYun <yunyatkin95@gmail.com>
2024-09-06 19:49:27 +0300

    test

--- react-burger/cypress/e2e/maintest.cy.js
+++ react-burger/cypress/e2e/maintest.cy.js
@@ -25,29 +25,32 @@ describe('main test scenario', () => {
   });
 
   // создадим константы
-  const bun = `[data-testid="bun"]`;
-  const ingredient = `[data-testid="ingredient"]`;
-  const dropZoneTop = `[data-testid="dropzone-top"]`;
-  const dropZoneMiddle = `[data-testid="dropzone-middle"]`;
-  const modal = `[data-testid="modal-overlay"]`;
+  const bun = '[data-testid="bun"]';
+  const ingredient = '[data-testid="ingredient"]';
+  const dropZoneTop = '[data-testid="dropzone-top"]';
+  const dropZoneMiddle = '[data-testid="dropzone-middle"]';
+  const modal = '[data-testid="modal-overlay"]';
 
   // сценарий создания заказа
   it('should create order using react dnd', () => {
     // перемещаем булку в конструктор
-    cy.get(bun).first().trigger('dragstart').as('bun');
+    cy.get(bun).first().as('bun');
+    cy.get('@bun').trigger('dragstart');
     cy.get(dropZoneTop).trigger('drop').as('dropTop');
 
     // перемещаем ингредиенты
-    cy.get(ingredient).first().trigger('dragstart').as('ingredient');
+    cy.get(ingredient).first().as('ingredient');
+    cy.get('@ingredient').trigger('dragstart');
     cy.get(dropZoneMiddle).trigger('drop').as('dropMiddle');
 
-    cy.get('@ingredient').eq(1).trigger('dragstart');
+    cy.get(ingredient).eq(1).trigger('dragstart');
     cy.get('@dropMiddle').trigger('drop');
   });
   // проверка модального окна
   it('should check modal', () => {
     // Открываем модальное окно при нажатии на ингредиент
-    cy.get('@ingredient').first().click();
+    cy.get(ingredient).first().as('ingredient');
+    cy.get('@ingredient').click();
 
     // Проверяем, что модальное окно открылось и содержит данные ингредиента
     cy.get(modal).should('exist').as('modal');
@@ -60,10 +63,15 @@ describe('main test scenario', () => {
   });
   // отправка заказа и получение номера заказа
   it('should send order', () => {
-    cy.get('@bun').first().trigger('dragstart');
-    cy.get('@dropTop').trigger('drop');
-    cy.get('@ingredient').first().trigger('dragstart');
-    cy.get('@dropMiddle').trigger('drop');
+    // Drag and drop bun
+    cy.get(bun).first().as('bun');
+    cy.get('@bun').trigger('dragstart');
+    cy.get(dropZoneTop).trigger('drop');
+
+    // Drag and drop ingredient
+    cy.get(ingredient).first().as('ingredient');
+    cy.get('@ingredient').trigger('dragstart');
+    cy.get(dropZoneMiddle).trigger('drop');
 
     // Нажимаем кнопку оформления заказа
     cy.get('button').contains('Оформить заказ').click();
@@ -72,7 +80,7 @@ describe('main test scenario', () => {
     cy.wait('@postOrder');
 
     // Проверяем, что открывается модальное окно с номером заказа
-    cy.get('@modal').should('exist');
+    cy.get(modal).should('exist');
     cy.get('[data-testid="order-number"]').should('contain.text', '123'); // номер из fixture -> order
   });
 });
